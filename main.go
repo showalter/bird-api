0e037f0ff3422937cb8006cc6fae465c6edd6b2a
Ryan Showalter <showalrm@dukes.jmu.edu>
2022-04-06 16:35:01 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Bird struct {
+	Image       string `json:"image"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func main() {
+	app := fiber.New()
+
+	buf, err := ioutil.ReadFile("birds.json")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	s := string(buf)
+
+	var birds []Bird
+	json.Unmarshal([]byte(s), &birds)
+
+	fmt.Printf("Birds : %+v", birds)
+
+	randSource := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(randSource)
+
+	app.Get("/bird", func(c *fiber.Ctx) error {
+		return c.JSON(birds[random.Intn(len(birds))])
+	})
+
+	app.Listen(":2999")
+}
